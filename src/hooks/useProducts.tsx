6a443adc91a75b1bd60876d7e1fc2afdf65d449f
Paulo-Moncaio <paulomoncaio@gmail.com>
2024-02-12 17:39:21 -0300

    custom hook to fetch products from api

---
+++ src/hooks/useProducts.tsx
@@ -0,0 +1,32 @@
+'use client'
+import { useState, useEffect } from 'react';
+
+export type ProductsResponse = {
+  id: number | string;
+  title: string;
+  platform: string;
+  price: number;
+  image: string;
+};
+
+const useFetchProducts = () => {
+  const [products, setProducts] = useState<ProductsResponse[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/items');
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return products;
+};
+
+export default useFetchProducts;
