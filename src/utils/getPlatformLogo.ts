70f9ff7dd5124ce3e9377711d678f7bbc5149caa
Paulo-Moncaio <paulomoncaio@gmail.com>
2024-02-12 17:38:45 -0300

    function to get platform logo

---
+++ src/utils/getPlatformLogo.ts
@@ -0,0 +1,13 @@
+type Platform = 'Steam' | 'PS4' | 'Nintendo Switch' | string;
+
+export function getPlatformLogo(platform: Platform): string {
+  const platformLogos: Record<Platform, string> = {
+    'Steam': '/Steam_logo_black.png',
+    'PS4': '/PlayStation_black_logo.png',
+    'Nintendo Switch': '/Nintendo-Switch-Logo.png',
+  };
+
+  const defaultLogo = '/default_logo.png';
+
+  return platformLogos[platform] || defaultLogo;
+}
